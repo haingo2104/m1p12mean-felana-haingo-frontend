c96f25cd41e78c1716213c87f86d18723ca1cf1e
felana-niaina <nirina.felananiaina@gmail.com>
2025-03-12 17:35:14 +0300

    creation page home & login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TestUtilisationComponent } from '../components/test-utilisation/test-utilisation.component';
+import { HomeComponent } from '../components/home/home.component';
 
 export const routes: Routes = [
-    {path : 'testUtilisation' , component : TestUtilisationComponent}
+    {path : 'testUtilisation' , component : TestUtilisationComponent},
+    {path : 'home' , component : HomeComponent}
 ];
