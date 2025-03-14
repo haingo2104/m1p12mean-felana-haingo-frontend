d109c105a3cdee4f8817370b7462a196aa8ce798
felana-niaina <nirina.felananiaina@gmail.com>
2025-03-14 16:36:00 +0300

    liaison front-back addUser

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SendUserService } from '../../services/SendUserService';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone : true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  data = {
+    name :'',
+    email : '',
+    phone:'1224455',
+    password : '',
+    role:'client'
+  }
 
+  constructor(private readonly apiService: SendUserService){}
+  onSubmit(){
+    if(this.data) {
+      this.apiService.sendData({ data: this.data}).subscribe({
+        next: (response) => {
+          console.log("Réponse de l'API:", response);
+        },
+        error: (error) =>{
+          
+          console.log('data service',this.data)
+          console.error('Erreur', error);
+        }
+      })
+    }
+  }
 }
