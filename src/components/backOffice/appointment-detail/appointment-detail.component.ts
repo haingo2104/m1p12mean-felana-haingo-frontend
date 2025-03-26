8382d267b7c35ccca2ae17248fbee4eb35d6e57d
haingo2104 <haingo.faramampionona@orchisintl.com>
2025-03-26 16:59:28 +0300

    appointment list configuration

--- src/components/backOffice/appointment-detail/appointment-detail.component.ts
+++ src/components/backOffice/appointment-detail/appointment-detail.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit, SimpleChanges } from '@angular/core';
+import { AppointmentServiceService } from '../../../services/appointment-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-appointment-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './appointment-detail.component.html',
   styleUrl: './appointment-detail.component.css'
 })
-export class AppointmentDetailComponent {
-
+export class AppointmentDetailComponent implements OnInit{
+  appointmentId = input<string>('');     
+  appointmentDetails: any | null = null;   
+  constructor(private readonly appointService: AppointmentServiceService) {}
+  ngOnInit(): void {
+    if (this.appointmentId) {
+      this.fetchAppointmentDetail(this.appointmentId());
+    }
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['appointmentId'] && changes['appointmentId'].currentValue) {
+      this.fetchAppointmentDetail(changes['appointmentId'].currentValue);
+    }
+  }
+  fetchAppointmentDetail(appointmentId: string): void {
+    this.appointService.fetchAppointmentDetail(appointmentId).subscribe(details => {
+      this.appointmentDetails = details;
+    });
+  }
 }
--- src/components/backOffice/appointment-list/appointment-list.component.ts
+++ src/components/backOffice/appointment-list/appointment-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AppointmentServiceService } from '../../../services/appointment-service.service';
 import { CommonModule } from '@angular/common';
+import { AppointmentDetailComponent } from '../appointment-detail/appointment-detail.component';
 
 @Component({
   selector: 'app-appointment-list',
-  imports: [CommonModule],
+  imports: [CommonModule , AppointmentDetailComponent],
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
 export class AppointmentListComponent implements OnInit {
   appointments: any[] = [];
+  selectedAppointmentId: string | null = null;
+
   constructor(
     private readonly appointmentService: AppointmentServiceService
   ) {}
@@ -17,5 +20,19 @@ export class AppointmentListComponent implements OnInit {
     this.appointmentService.getAllAppointments().subscribe((appointments) => {
       this.appointments = appointments;
     });
+  }
+  selectAppointment(appointmentId: string): void {
+    if (!appointmentId) {
+      console.error('Invalid book ID');
+      return;
+    }
+    
+    if (this.selectedAppointmentId === appointmentId) {
+      this.selectedAppointmentId = ''; 
+    } else {
+      this.selectedAppointmentId = appointmentId; 
+    }
+  
+    console.log("Selected book ID:", this.selectedAppointmentId);
   }
 }
