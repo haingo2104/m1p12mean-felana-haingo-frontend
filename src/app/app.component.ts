3cdb5d0a4a0c155c9f76d0ee8170798e9a0da72c
haingo2104 <haingo.faramampionona@orchisintl.com>
2025-03-27 15:45:51 +0300

    layout configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../components/backOffice/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../components/backOffice/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , SidebarComponent , CommonModule , HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'm1p12mean-felana-haingo-frontend';
+  showSidebar: boolean = true;
+
+  hiddenRoutes: string[] = ['/', '/login']; 
+  constructor(private readonly router: Router) {
+    this.router.events.subscribe(() => {
+      this.showSidebar = !this.hiddenRoutes.includes(this.router.url);
+    });
+  }
 }
